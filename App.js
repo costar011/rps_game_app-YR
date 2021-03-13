9a74475541643f953f3d6a06afae9f97663393e8
YERIM <dnaer456@gmail.com>
2021-03-13 14:43:25 +0900

    test

--- App.js
+++ App.js
@@ -3,12 +3,29 @@ import { StyleSheet, Text, View } from "react-native";
 
 const App = () => {
   return (
-    <View>
-      <View>
-        <Text>Test</Text>
+    <View style={styles.container}>
+      <View style={styles.view2}>
+        <Text style={styles.txt}>Test</Text>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: `#2f3dc2`,
+  },
+  view2: {
+    flex: 1,
+    alignItems: `center`,
+    justifyContent: `center`,
+  },
+  txt: {
+    fontSize: 20,
+    fontWeight: `600`,
+    color: `#fff`,
+  },
+});
 
 export default App;
