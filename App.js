edb1bb3c9dfa449925272640a70e574cb20322c1
YERIM <dnaer456@gmail.com>
2021-03-13 15:26:46 +0900

    btn make

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Dimensions,
+  /* 현재 디바이스의 width 값 가져오기 - Dimensions */
+} from "react-native";
+
+import { LinearGradient } from "expo-linear-gradient";
+
+const CURRENT_WIDTH = Dimensions.get(`window`).width;
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.view2}>
-        <Text style={styles.txt}>Test</Text>
+      <View style={styles.TopBox}>
+        <TouchableOpacity>
+          <View style={styles.BtnBox}></View>
+        </TouchableOpacity>
       </View>
+      <View style={styles.DownBox}></View>
     </View>
   );
 };
@@ -14,17 +28,36 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: `#2f3dc2`,
   },
-  view2: {
-    flex: 1,
+
+  TopBox: {
+    flex: 8,
+
     alignItems: `center`,
     justifyContent: `center`,
+
+    backgroundColor: `#7cebe0`,
   },
-  txt: {
-    fontSize: 20,
-    fontWeight: `600`,
-    color: `#fff`,
+
+  DownBox: {
+    flex: 2,
+
+    alignItems: `center`,
+    justifyContent: `center`,
+
+    backgroundColor: `#16c9c3`,
+  },
+
+  BtnBox: {
+    width: CURRENT_WIDTH / 2,
+    height: 45,
+    backgroundColor: "#5148f7",
+    borderRadius: 5,
+
+    alignItems: "center",
+    justifyContent: "center",
+
+    marginBottom: 50,
   },
 });
 
