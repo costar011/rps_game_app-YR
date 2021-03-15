eac90d1887c630f5ff41a185c48d71e496186d8f
YERIM <dnaer456@gmail.com>
2021-03-15 15:26:50 +0900

    img

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -11,18 +11,64 @@ import {
 import { LinearGradient } from "expo-linear-gradient";
 
 const CURRENT_WIDTH = Dimensions.get(`window`).width;
+const RPS_IMAGE = [
+  "http://i.011st.com/cm_exhibition/2012/1501/mwevent/game/images/user_img_0.png",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEqumKKO3S5YxpDvwSclqci_cvGC1_HRKimg&usqp=CAU",
+  "http://image.blog.livedoor.jp/deaiup/imgs/1/5/1506be2c.png",
+];
 
 const App = () => {
+  const [tab, setTab] = useState(0);
+  const [MeData, setMeData] = useState(`잠시만 기다려주세요.`);
+  const [ComData, setComData] = useState(`잠시만 기다려주세요.`);
+  const [resultText, setValue] = useState(``);
+  const [meImage, setMeImage] = useState(``);
+  const [comImage, setComImage] = useState(``);
+
+  const RanDom = () => Math.floor(Math.random() * 3);
+
+  const StartBtnHandler = (value) => {
+    setTab(value);
+
+    if (value === 0) {
+      setMeData(`잠시만 기다려 주세요`);
+      setComData(`잠시만 기다려 주세요`);
+      setValue(``);
+    }
+
+    if (value === 1) {
+      const rum1 = StartBtnHandler();
+      const rum2 = StartBtnHandler();
+
+      const me = RPS_IMAGE[rum1];
+      const com = RPS_IMAGE[rum2];
+
+      const setMeImage = RPS_IMAGE[rum1];
+      const setComImage = RPS_IMAGE[rum2];
+
+      setMeData(me);
+      setComData(com);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.TopBox}>
+        {tab === 0 && (
+          <TouchableOpacity onPressOut={() => StartBtnHandler(1)}>
+            <View style={styles.BtnBox}>
+              <Text style={styles.Txt}>START</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      </View>
+      <View style={styles.DownBox}>
         <TouchableOpacity>
           <View style={styles.BtnBox}>
-            <Text style={styles.Txt}>Start</Text>
+            <Text style={styles.Txt}>AGAIN</Text>
           </View>
         </TouchableOpacity>
       </View>
-      <View style={styles.DownBox}></View>
     </View>
   );
 };
@@ -37,8 +83,6 @@ const styles = StyleSheet.create({
 
     alignItems: `center`,
     justifyContent: `center`,
-
-    backgroundColor: `#9ca0a1`,
   },
 
   DownBox: {
